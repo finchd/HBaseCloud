09ca99e88f32a9ed6dd27cf64988862a5d016421
Sean Mahan <mahan2@pdx.edu>
2016-03-02 20:20:17 -0800

    next attempt

--- src/main/java/hello/QueryNumSpeedsOver100.java
+++ src/main/java/hello/QueryNumSpeedsOver100.java
@@ -40,7 +40,7 @@ public class QueryNumSpeedsOver100 {
 
         try
         {
-            Configuration config = HBaseConfiguration.create();
+            /*Configuration config = HBaseConfiguration.create();
             Job job = Job.getInstance(config, "PageViewCounts");
             job.setJarByClass(QueryNumSpeedsOver100.class);     // class that contains mapper and reducer
 
@@ -67,6 +67,43 @@ public class QueryNumSpeedsOver100 {
            // if (!b) {
             //    throw new IOException("error with job!");
             //}
+            // Setup Hadoop
+            */
+            Configuration conf = HBaseConfiguration.create();
+            Job job = Job.getInstance(conf, "PageViewCounts");
+            job.setJarByClass( QueryNumSpeedsOver100.class );
+
+            // Create a scan
+            Scan scan = new Scan();
+
+            // Configure the Map process to use HBase
+            TableMapReduceUtil.initTableMapperJob(
+
+                    "freeway_loopdata",                    // The name of the table
+                    scan,                           // The scan to execute against the table
+                    MyMapper.class,                 // The Mapper class
+                    LongWritable.class,             // The Mapper output key class
+                    LongWritable.class,             // The Mapper output value class
+                    job );                          // The Hadoop job
+
+            // Configure the reducer process
+            job.setReducerClass( MyReducer.class );
+            job.setCombinerClass( MyReducer.class );
+
+            // Setup the output - we'll write to the file system: HOUR_OF_DAY   PAGE_VIEW_COUNT
+            job.setOutputKeyClass( LongWritable.class );
+            job.setOutputValueClass( LongWritable.class );
+            job.setOutputFormatClass( TextOutputFormat.class );
+
+            // We'll run just one reduce task, but we could run multiple
+            job.setNumReduceTasks( 1 );
+
+            // Write the results to a file in the output directory
+            FileOutputFormat.setOutputPath( job, new Path( "output" ) );
+
+            // Execute the job
+            System.exit( job.waitForCompletion( true ) ? 0 : 1 );
+
         }
         catch( Exception e )
         {
@@ -109,6 +146,49 @@ public class QueryNumSpeedsOver100 {
             context.write(null, put);
         }
     }
+
+    static class MyMapper extends TableMapper<LongWritable, LongWritable> {
+        private LongWritable ONE = new LongWritable(1);
+
+        public MyMapper() {
+        }
+
+        @Override
+        protected void map(ImmutableBytesWritable rowkey, Result columns, Mapper.Context context) throws IOException, InterruptedException
+
+        {
+
+            Integer speed = Integer.getInteger(Bytes.toString(columns.getValue("freeway_loopdata".getBytes(), "speed".getBytes())));
+            if (speed > 100){
+                context.write(new LongWritable(100), ONE);
+            }
+
+        }
+
+    }
+
+    static class MyReducer extends Reducer<LongWritable, LongWritable, LongWritable, LongWritable>
+
+    {
+        public MyReducer() {
+        }
+
+        @Override
+        protected void reduce(LongWritable key, Iterable<LongWritable> values, Context context) throws IOException, InterruptedException
+
+        {
+            // Add up all of the page views for this hour
+            long sum = 0;
+            for( LongWritable count : values )
+            {
+                sum += count.get();
+            }
+
+            // Write out the current hour and the sum
+            context.write( key, new LongWritable( sum ) );
+        }
+    }
+
 
 }
 
