9b7858a58198b7fa92b59cec745f9dd588064bc3
Donovan Finch <finchd@server.fake>
2016-03-04 15:53:07 -0800

    Add template code to Peak Travel Time query.
    Will rely on a seperate Java project that implements the MapReduce job
    described.

--- src/main/java/hello/QueryPeakTravelTimeFosterNB.java
+++ src/main/java/hello/QueryPeakTravelTimeFosterNB.java
@@ -5,6 +5,18 @@ package hello;
  */
 public class QueryPeakTravelTimeFosterNB {
     public String getResult(){
+
+        // Map: takes a locationID (Foster NB) and (locationID, detectorID, timestamp, speed) tuples and outputs only
+        // tuples that match the location (Foster NB) and time.
+        // Reduce: sums speed, then average by dividing by station length
+
+
+        Configuration conf = HBaseConfiguration.create();
+        HTable table = new HTable(conf, "highways"); // TODO: change table to results from MapReduce job
+        //Scan scan = new Scan();
+
+        // Here, just query output table from MapReduce, and post-process (ie sorting)
+
         return "Query 3 result";
     }
 }
