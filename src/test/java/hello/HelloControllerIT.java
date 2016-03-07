6e2de2d82d8be830aba4b81f1e79df1eb4a18393
Sean Mahan <mahan2@pdx.edu>
2016-03-07 11:40:53 -0800

    fix tests

--- src/test/java/hello/HelloControllerIT.java
+++ src/test/java/hello/HelloControllerIT.java
@@ -43,7 +43,7 @@ public class HelloControllerIT {
 	@Test
 	public void getQuery1() throws Exception {
 		ResponseEntity<String> response = template.getForEntity(base.toString()  + "query1/", String.class);
-		assertThat(response.getBody(), equalTo("<h1>Speeders on I-205</h1><br>Speeds over 50: 8571269\tPercentage of total: 60.9645%<br>Speeds over 60: 4412134\tPercentage of total: 31.382%<br>Speeds over 70: 484173\tPercentage of total: 3.4438%<br>Speeds over 80: 99708\tPercentage of total: 0.7092%<br>Speeds over 90: 30821\tPercentage of total: 0.2192%<br>Speeds over 100: 6972\tPercentage of total: 0.0496%<br>Speeds over 102: 5533\tPercentage of total: 0.0394%<br>Speeds over 104: 2728\tPercentage of total: 0.0194%<br>Speeds over 106: 279\tPercentage of total: 0.002%<br><br>Avg speed: 41.0 Total speed records 14059453"));
+		assertThat(response.getBody(), equalTo("<h1>Speeders on I-205</h1><br><table border=\"1\"><tr><td>Speed</td><td># greater than</td><td>percent of total</td></td><tr><td>50<td/><td>8571269</td><td>60.9645</td></td><tr><td>60<td/><td> 4412134</td><td>31.382</td></td><tr><td>70<td/><td>484173</td><td>3.4438</td></td><tr><td>80<td/><td>99708</td><td>0.7092</td></td><tr><td>90<td/><td>30821</td><td>0.2192</td></td><tr><td>100<td/><td>6972</td><td>0.0496</td></td><tr><td>102<td/><td>5533</td><td>0.0394</td></td><tr><td>104<td/><td>2728</td><td>0.0194</td></td><tr><td>106<td/><td>279</td><td>0.002</td></td></table><br>Avg speed: 41.0 Total speed records 14059453"));
 	}
 	@Test
 	public void getQuery2() throws Exception {
