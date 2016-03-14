2c6be71ef66cab19ba978c16281b6e6b0827d6a5
Mierzwik <b.mierzwik@gmail.com>
2016-03-14 14:25:38 -0700

    fixed html for springboot page

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -15,11 +15,11 @@ public class HelloController {
     String SpringBootPage() {
         String ret = "<h1>HBase Queries</h1>";
         ret += "<ul>";
-        ret += "<li>Query 1: <a href =\"/query1/\" Number of speeds over 100</a></li>";
-        ret += "<li>Query 2: <a href =\"/query2/\" Total volume for Foster NB</a></li>";
-        ret += "<li>Query 3: <a href =\"/query3/\" Peak Travel Time Foster NB</a></li>";
-        ret += "<li>Query 4: <a href =\"/query4/\" Route from Johnson Creek to Columbia</a></li>";
-        ret += "<li>Query 5: <a href =\"/query 5/\" Update Station ID Length</a></li>";
+        ret += "<li>Query 1: <a href =\"/query1/\"> Number of speeds over 100</a></li>";
+        ret += "<li>Query 2: <a href =\"/query2/\"> Total volume for Foster NB</a></li>";
+        ret += "<li>Query 3: <a href =\"/query3/\"> Peak Travel Time Foster NB</a></li>";
+        ret += "<li>Query 4: <a href =\"/query4/\"> Route from Johnson Creek to Columbia</a></li>";
+        ret += "<li>Query 5: <a href =\"/query 5/\"> Update Station ID Length</a></li>";
         ret += "</ul>";
         return ret;
     }
